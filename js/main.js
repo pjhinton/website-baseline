63b9c29512418c6d16bd392ff8fb2867b5c050de
P.J. Hinton <pjhinton@aim.com>
2013-03-12 03:13:52 +0000

    import baseline direcotry tree
    This commit contains the initial import of a minimal website
    directory tree.  Included in this tree are:
    
    * Backbone 0.9.10
    * Twitter Bootstrap 2.3.1
    * jQuery 1.9.1
    * LESS 1.3.3
    * Mustache (JavaScript) 0.7.2
    * Underscore 1.4.4
    * Font Awesome 3.0.2
    
    All scripts and CSS files are minified versions.

---
+++ js/main.js
@@ -0,0 +1,37 @@
+requirejs.config(
+	{
+		shim: {
+			"util/backbone": {
+				deps: ["util/jquery", "util/underscore"],
+				exports: "Backbone"
+			},
+			"util/bootstrap": {
+				deps: ["util/jquery"]
+			},
+			"util/jquery" : {
+				exports: "$"
+			},
+			"util/underscore": {
+				exports: "_"
+			}
+		},
+	}
+);
+
+require(
+
+	["util/jquery", "util/bootstrap", "util/underscore", "util/backbone", "util/mustache",
+		"text!templates/hello.mustache"],
+
+	function ($, bootstrapUndefined, _, Backbone, Mustache, hello) {
+
+		$(document).ready(
+			function () {
+				console.log("Document has fired the 'ready' event.");
+				$("#hello").html(Mustache.render(hello, {name: "world"}));
+			}
+		);
+
+	}
+
+);
